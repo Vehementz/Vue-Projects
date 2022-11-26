c99bc2887b0a5fc971f3e0b4fc4536bb0772d172
Vehementz <vale-tudo91@sfr.fr>
2022-11-26 20:41:19 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction timer</h1>
+  <button @click="start">Play</button>
+  <TimerBlock v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TimerBlock from "./components/TimerBlock.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { TimerBlock},
+  data() {
+    return {
+     isPlaying: false, 
+     delay: null,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    }
   }
 }
 </script>
@@ -20,7 +33,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #7fb2e6;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/TimerBlock.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="block">
+        Click here
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: "TimerBlock",
+    props: ['delay']
+       
+    
+
+}
+
+</script>
+
+<style>
+
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: rgb(161, 226, 161);
+    color: black;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+
+</style>
